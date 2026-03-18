a25f8665e13d974dbe8471653ebae717e414b1ca
Pham Minh Duy <minhduy3926@gmail.com>
2026-03-18 13:40:55 +0700

    fix JDBC

--- src/Dao/JDBCUtil.java
+++ src/Dao/JDBCUtil.java
@@ -4,26 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class JDBCUtil {
-    private static final String url = "jdbc:mysql://localhost:3306/quanlycuahangbansach?useUnicode=true&characterEncoding=UTF-8";
-    private static final String user = "root";
-    private static final String password = ""; // Điền mật khẩu MySQL 
-    public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Lỗi: Không tìm thấy Driver kết nối MySQL");
-        }
-        return DriverManager.getConnection(url, user, password);
+public class ConnectDB {
+    private static final String url="jdbc:mysql://localhost:3306/qlbansach?useSSL=false&serverTimezone=UTC";
+    private static final String user="root";
+    private static final String password="";
+    //lấy kết nối
+public static Connection getConnection() throws SQLException{
+    try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
     }
-
-    public static void closeConnection(Connection con) {
-        try {
-            if (con != null && !con.isClosed()) {
-                con.close();
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+    catch(ClassNotFoundException e){
+        System.out.print("không thể kết nối đc");
     }
+    return DriverManager.getConnection(url,user,password);
+}
+public static void close(Connection con){
+    try{
+          if(con != null) con.close();
+    }catch(SQLException ex){
+        ex.printStackTrace();
+    }
+}
 }
